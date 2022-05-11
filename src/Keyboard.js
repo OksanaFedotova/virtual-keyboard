845b3bf150392f615a32bc9e536eee1ad261eab2
Oksana Fedotova <oxana-fedotova@yandex.ru>
2022-05-11 21:26:51 +0300

    fix: add airbnb-rules to linter and fix some key highlits

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,20 +1,35 @@
+/* eslint-disable no-unused-expressions */
 import { createKey } from './createKey.js';
 
-export class Keyboard {
-    constructor(layout, language) {
-        this.layout = layout;
-        this.language = language;
-        this.keysContainer = ''
+export default class Keyboard {
+  constructor(layout, language) {
+    this.layout = layout;
+    this.language = language;
+    this.keysContainer = '';
+    this.actives = false;
+  }
+
+  init() {
+    this.keysContainer = document.createElement('div');
+    const keys = this.layout.map((keyData) => createKey(keyData, this.language));
+    if (this.actives) {
+      this.actives.forEach((active) => {
+        const attr = active.dataset.code;
+        const keyActive = document.querySelector(`[data-code=${attr}]`);
+        keyActive.classList.add('active');
+      });
     }
-    init() {
-        this.keysContainer = document.createElement("div");
-        const keys = this.layout.map((keyData) => createKey(keyData, this.language));
-        keys.forEach((key) => {
-        const insertLineBreak = ['Backspace', 'Enter', 'Backslash', 'ShiftRight'].indexOf(key.dataset.code) !== -1;
-        insertLineBreak? this.keysContainer.append(key, document.createElement("br")):  this.keysContainer.append(key);
-        })      
-    }
-    getContainer() {
-        return this.keysContainer
-    }
+    keys.forEach((key) => {
+      const insertLineBreak = ['Backspace', 'Enter', 'Backslash', 'ShiftRight'].indexOf(key.dataset.code) !== -1;
+      insertLineBreak ? this.keysContainer.append(key, document.createElement('br')) : this.keysContainer.append(key);
+    });
+  }
+
+  setActives(actives) {
+    this.actives = actives;
+  }
+
+  getContainer() {
+    return this.keysContainer;
+  }
 }
--- src/keyboardLayout.js
+++ src/keyboardLayout.js
@@ -1,67 +1,196 @@
+/* eslint-disable import/prefer-default-export */
 export const keyboardLayout = [
-{keyEnglish: '§', keyCyrillic: ">", code: 'Backquote', shiftKey: false, shifted: '±', cyrillicShifted: '<'},
-{keyEnglish: '1', keyCyrillic: null, code: 'Digit1', shiftKey: false, shifted: '!'},
-{keyEnglish: '2', keyCyrillic: null, code: 'Digit2', shiftKey: false, shifted: '@', cyrillicShifted: '"'},
-{keyEnglish: '3', keyCyrillic: null, code: 'Digit3', shiftKey: false, shifted: '#', cyrillicShifted: '№'},
-{keyEnglish: '4', keyCyrillic: null, code: 'Digit4', shiftKey: false, shifted: '$', cyrillicShifted: '%'},
-{keyEnglish: '5', keyCyrillic: null, code: 'Digit5', shiftKey: false, shifted: '%', cyrillicShifted: ':'},
-{keyEnglish: '6', keyCyrillic: null, code: 'Digit6', shiftKey: false, shifted: '^', cyrillicShifted: ','},
-{keyEnglish: '7', keyCyrillic: null, code: 'Digit7', shiftKey: false, shifted: '&', cyrillicShifted: '.'},
-{keyEnglish: '8', keyCyrillic: null, code: 'Digit8', shiftKey: false, shifted: '*', cyrillicShifted: ';'},
-{keyEnglish: '9', keyCyrillic: null, code: 'Digit9', shiftKey: false, shifted: '('},
- {keyEnglish: '0', keyCyrillic: null, code: 'Digit0', shiftKey: false, shifted: ')'},
- {keyEnglish: '-', keyCyrillic: null, code: 'Minus', shiftKey: false, shifted: '_'},
- {keyEnglish: '=', keyCyrillic: null, code: 'Equal', shiftKey: false, shifted: '+'},
-    {keyEnglish: 'Backspace', keyCyrillic: null, code: 'Backspace', shiftKey: false},
-    {keyEnglish: 'Tab', keyCyrillic: null, code: 'Tab', shiftKey: false},
-    {keyEnglish: 'q', keyCyrillic: 'й', code: 'KeyQ', shiftKey: false},
-    {keyEnglish: 'w', keyCyrillic: 'ц', code: 'KeyW', shiftKey: false},
-    {keyEnglish: 'e', keyCyrillic: 'у', code: 'KeyE', shiftKey: false},
-    {keyEnglish: 'r', keyCyrillic: 'к', code: 'KeyR', shiftKey: false},
-    {keyEnglish: 't', keyCyrillic: 'е', code: 'KeyT', shiftKey: false},
-    {keyEnglish: 'y', keyCyrillic: 'н', code: 'KeyY', shiftKey: false},
-    {keyEnglish: 'u', keyCyrillic: 'г', code: 'KeyU', shiftKey: false},
-    {keyEnglish: 'i', keyCyrillic: 'ш', code: 'KeyI', shiftKey: false},
-    {keyEnglish: 'o', keyCyrillic: 'щ', code: 'KeyO', shiftKey: false},
-    {keyEnglish: 'p', keyCyrillic: 'з', code: 'KeyP', shiftKey: false},
-    {keyEnglish: '[', keyCyrillic: 'х', code: 'BracketLeft', shiftKey: false, shifted: '{'},
-    {keyEnglish: ']', keyCyrillic: 'ъ', code: 'BracketRight', shiftKey: false, shifted: '}'},
-    {keyEnglish: 'Enter', keyCyrillic: null, code: 'Enter', shiftKey: false},
-    {keyEnglish: 'CapsLock', keyCyrillic: null, code: 'CapsLock', shiftKey: false},
-    {keyEnglish: 'a', keyCyrillic: 'ф', code: 'KeyA', shiftKey: false},
-    {keyEnglish: 's', keyCyrillic: 'ы', code: 'KeyS', shiftKey: false},
-    {keyEnglish: 'd', keyCyrillic: 'в', code: 'KeyD', shiftKey: false},
-    {keyEnglish: 'f', keyCyrillic: 'а', code: 'KeyF', shiftKey: false},
-    {keyEnglish: 'g', keyCyrillic: 'п', code: 'KeyG', shiftKey: false},
-    {keyEnglish: 'h', keyCyrillic: 'р', code: 'KeyH', shiftKey: false},
-    {keyEnglish: 'j', keyCyrillic: 'о', code: 'KeyJ', shiftKey: false},
-    {keyEnglish: 'k', keyCyrillic: 'л', code: 'KeyK', shiftKey: false},
-    {keyEnglish: 'l', keyCyrillic: 'д', code: 'KeyL', shiftKey: false},
-    {keyEnglish: ';', keyCyrillic: 'ж', code: 'Semicolon', shiftKey: false, shifted: ':'},
-    {keyEnglish: "'", keyCyrillic: 'э', code: 'Quote', shiftKey: false, shifted: '"'},
-    {keyEnglish: '\\', keyCyrillic: 'ё', code: 'Backslash', shiftKey: false, shifted: '|'},
-    {keyEnglish: 'Shift', keyCyrillic: null, code: 'ShiftLeft', shiftKey: true},
-    {keyEnglish: '`', keyCyrillic: ']', code: 'IntlBackslash', shiftKey: false, shifted: '~',  cyrillicShifted: '['},
-    {keyEnglish: 'z', keyCyrillic: 'я', code: 'KeyZ', shiftKey: false},
-    {keyEnglish: 'x', keyCyrillic: 'ч', code: 'KeyX', shiftKey: false},
-    {keyEnglish: 'c', keyCyrillic: 'с', code: 'KeyC', shiftKey: false},
-    {keyEnglish: 'v', keyCyrillic: 'м', code: 'KeyV', shiftKey: false},
-    {keyEnglish: 'b', keyCyrillic: 'и', code: 'KeyB', shiftKey: false},
-    {keyEnglish: 'n', keyCyrillic: 'т', code: 'KeyN', shiftKey: false},
-    {keyEnglish: 'm', keyCyrillic: 'ь', code: 'KeyM', shiftKey: false},
-    {keyEnglish: ',', keyCyrillic: 'б', code: 'Comma', shiftKey: false, shifted: '<'},
-    {keyEnglish: '.', keyCyrillic: 'ю', code: 'Period', shiftKey: false, shifted: '>'},
-    {keyEnglish: '/', keyCyrillic: '/', code: 'Slash', shiftKey: false, shifted: '?'},
-    {keyEnglish: 'Shift', keyCyrillic: null, code: 'ShiftRight', shiftKey: true},
-    {keyEnglish: 'Control', keyCyrillic: null, code: 'ControlLeft', shiftKey: false},
-    {keyEnglish: 'Alt', keyCyrillic: null, code: 'AltLeft', shiftKey: false},
-    {keyEnglish: 'Meta', keyCyrillic: null, code: 'MetaLeft', shiftKey: false},
-    {keyEnglish: ' ', keyCyrillic: null, code: 'Space', shiftKey: false},
-    {keyEnglish: 'Meta', keyCyrillic: null, code: 'MetaRight', shiftKey: false},
-    {keyEnglish: 'Alt', keyCyrillic: null, code: 'AltRight', shiftKey: false},
-    {keyEnglish: 'ArrowLeft', keyCyrillic: null, code: 'ArrowLeft', shiftKey: false},
-    {keyEnglish: 'ArrowUp', keyCyrillic: null, code: 'ArrowUp', shiftKey: false},
-    {keyEnglish: 'ArrowDown', keyCyrillic: null, code: 'ArrowDown', shiftKey: false},
-    {keyEnglish: 'ArrowRight', keyCyrillic: null, code: 'ArrowRight', shiftKey: false}
-]
-//const res = keyboardLayout.map((obj) => )
+  {
+    keyEnglish: '§', keyCyrillic: '>', code: 'Backquote', shiftKey: false, shifted: '±', cyrillicShifted: '<',
+  },
+  {
+    keyEnglish: '1', keyCyrillic: null, code: 'Digit1', shiftKey: false, shifted: '!',
+  },
+  {
+    keyEnglish: '2', keyCyrillic: null, code: 'Digit2', shiftKey: false, shifted: '@', cyrillicShifted: '"',
+  },
+  {
+    keyEnglish: '3', keyCyrillic: null, code: 'Digit3', shiftKey: false, shifted: '#', cyrillicShifted: '№',
+  },
+  {
+    keyEnglish: '4', keyCyrillic: null, code: 'Digit4', shiftKey: false, shifted: '$', cyrillicShifted: '%',
+  },
+  {
+    keyEnglish: '5', keyCyrillic: null, code: 'Digit5', shiftKey: false, shifted: '%', cyrillicShifted: ':',
+  },
+  {
+    keyEnglish: '6', keyCyrillic: null, code: 'Digit6', shiftKey: false, shifted: '^', cyrillicShifted: ',',
+  },
+  {
+    keyEnglish: '7', keyCyrillic: null, code: 'Digit7', shiftKey: false, shifted: '&', cyrillicShifted: '.',
+  },
+  {
+    keyEnglish: '8', keyCyrillic: null, code: 'Digit8', shiftKey: false, shifted: '*', cyrillicShifted: ';',
+  },
+  {
+    keyEnglish: '9', keyCyrillic: null, code: 'Digit9', shiftKey: false, shifted: '(',
+  },
+  {
+    keyEnglish: '0', keyCyrillic: null, code: 'Digit0', shiftKey: false, shifted: ')',
+  },
+  {
+    keyEnglish: '-', keyCyrillic: null, code: 'Minus', shiftKey: false, shifted: '_',
+  },
+  {
+    keyEnglish: '=', keyCyrillic: null, code: 'Equal', shiftKey: false, shifted: '+',
+  },
+  {
+    keyEnglish: 'Backspace', keyCyrillic: null, code: 'Backspace', shiftKey: false,
+  },
+  {
+    keyEnglish: 'Tab', keyCyrillic: null, code: 'Tab', shiftKey: false,
+  },
+  {
+    keyEnglish: 'q', keyCyrillic: 'й', code: 'KeyQ', shiftKey: false,
+  },
+  {
+    keyEnglish: 'w', keyCyrillic: 'ц', code: 'KeyW', shiftKey: false,
+  },
+  {
+    keyEnglish: 'e', keyCyrillic: 'у', code: 'KeyE', shiftKey: false,
+  },
+  {
+    keyEnglish: 'r', keyCyrillic: 'к', code: 'KeyR', shiftKey: false,
+  },
+  {
+    keyEnglish: 't', keyCyrillic: 'е', code: 'KeyT', shiftKey: false,
+  },
+  {
+    keyEnglish: 'y', keyCyrillic: 'н', code: 'KeyY', shiftKey: false,
+  },
+  {
+    keyEnglish: 'u', keyCyrillic: 'г', code: 'KeyU', shiftKey: false,
+  },
+  {
+    keyEnglish: 'i', keyCyrillic: 'ш', code: 'KeyI', shiftKey: false,
+  },
+  {
+    keyEnglish: 'o', keyCyrillic: 'щ', code: 'KeyO', shiftKey: false,
+  },
+  {
+    keyEnglish: 'p', keyCyrillic: 'з', code: 'KeyP', shiftKey: false,
+  },
+  {
+    keyEnglish: '[', keyCyrillic: 'х', code: 'BracketLeft', shiftKey: false, shifted: '{',
+  },
+  {
+    keyEnglish: ']', keyCyrillic: 'ъ', code: 'BracketRight', shiftKey: false, shifted: '}',
+  },
+  {
+    keyEnglish: 'Enter', keyCyrillic: null, code: 'Enter', shiftKey: false,
+  },
+  {
+    keyEnglish: 'CapsLock', keyCyrillic: null, code: 'CapsLock', shiftKey: false,
+  },
+  {
+    keyEnglish: 'a', keyCyrillic: 'ф', code: 'KeyA', shiftKey: false,
+  },
+  {
+    keyEnglish: 's', keyCyrillic: 'ы', code: 'KeyS', shiftKey: false,
+  },
+  {
+    keyEnglish: 'd', keyCyrillic: 'в', code: 'KeyD', shiftKey: false,
+  },
+  {
+    keyEnglish: 'f', keyCyrillic: 'а', code: 'KeyF', shiftKey: false,
+  },
+  {
+    keyEnglish: 'g', keyCyrillic: 'п', code: 'KeyG', shiftKey: false,
+  },
+  {
+    keyEnglish: 'h', keyCyrillic: 'р', code: 'KeyH', shiftKey: false,
+  },
+  {
+    keyEnglish: 'j', keyCyrillic: 'о', code: 'KeyJ', shiftKey: false,
+  },
+  {
+    keyEnglish: 'k', keyCyrillic: 'л', code: 'KeyK', shiftKey: false,
+  },
+  {
+    keyEnglish: 'l', keyCyrillic: 'д', code: 'KeyL', shiftKey: false,
+  },
+  {
+    keyEnglish: ';', keyCyrillic: 'ж', code: 'Semicolon', shiftKey: false, shifted: ':',
+  },
+  {
+    keyEnglish: "'", keyCyrillic: 'э', code: 'Quote', shiftKey: false, shifted: '"',
+  },
+  {
+    keyEnglish: '\\', keyCyrillic: 'ё', code: 'Backslash', shiftKey: false, shifted: '|',
+  },
+  {
+    keyEnglish: 'Shift', keyCyrillic: null, code: 'ShiftLeft', shiftKey: true,
+  },
+  {
+    keyEnglish: '`', keyCyrillic: ']', code: 'IntlBackslash', shiftKey: false, shifted: '~', cyrillicShifted: '[',
+  },
+  {
+    keyEnglish: 'z', keyCyrillic: 'я', code: 'KeyZ', shiftKey: false,
+  },
+  {
+    keyEnglish: 'x', keyCyrillic: 'ч', code: 'KeyX', shiftKey: false,
+  },
+  {
+    keyEnglish: 'c', keyCyrillic: 'с', code: 'KeyC', shiftKey: false,
+  },
+  {
+    keyEnglish: 'v', keyCyrillic: 'м', code: 'KeyV', shiftKey: false,
+  },
+  {
+    keyEnglish: 'b', keyCyrillic: 'и', code: 'KeyB', shiftKey: false,
+  },
+  {
+    keyEnglish: 'n', keyCyrillic: 'т', code: 'KeyN', shiftKey: false,
+  },
+  {
+    keyEnglish: 'm', keyCyrillic: 'ь', code: 'KeyM', shiftKey: false,
+  },
+  {
+    keyEnglish: ',', keyCyrillic: 'б', code: 'Comma', shiftKey: false, shifted: '<',
+  },
+  {
+    keyEnglish: '.', keyCyrillic: 'ю', code: 'Period', shiftKey: false, shifted: '>',
+  },
+  {
+    keyEnglish: '/', keyCyrillic: '/', code: 'Slash', shiftKey: false, shifted: '?',
+  },
+  {
+    keyEnglish: 'Shift', keyCyrillic: null, code: 'ShiftRight', shiftKey: true,
+  },
+  {
+    keyEnglish: 'Control', keyCyrillic: null, code: 'ControlLeft', shiftKey: false,
+  },
+  {
+    keyEnglish: 'Alt', keyCyrillic: null, code: 'AltLeft', shiftKey: false,
+  },
+  {
+    keyEnglish: 'Meta', keyCyrillic: null, code: 'MetaLeft', shiftKey: false,
+  },
+  {
+    keyEnglish: ' ', keyCyrillic: null, code: 'Space', shiftKey: false,
+  },
+  {
+    keyEnglish: 'Meta', keyCyrillic: null, code: 'MetaRight', shiftKey: false,
+  },
+  {
+    keyEnglish: 'Alt', keyCyrillic: null, code: 'AltRight', shiftKey: false,
+  },
+  {
+    keyEnglish: 'ArrowLeft', keyCyrillic: null, code: 'ArrowLeft', shiftKey: false,
+  },
+  {
+    keyEnglish: 'ArrowUp', keyCyrillic: null, code: 'ArrowUp', shiftKey: false,
+  },
+  {
+    keyEnglish: 'ArrowDown', keyCyrillic: null, code: 'ArrowDown', shiftKey: false,
+  },
+  {
+    keyEnglish: 'ArrowRight', keyCyrillic: null, code: 'ArrowRight', shiftKey: false,
+  },
+];
+// const res = keyboardLayout.map((obj) => )
